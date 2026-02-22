980f8c8776844d9bb339f9b8d39d89387f581dbd
tanvir <md.tanvir.hossain.tusher@gmail.com>
2026-02-22 15:43:55 +0600

    Add RawSignalEvent dto to process signal from kafka

---
+++ trade-execution-service/src/main/java/com/doin/execution/payload/dto/RawSignalEvent.java
@@ -0,0 +1,19 @@
+package com.doin.execution.payload.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RawSignalEvent {
+    private String signalId;
+    private Long userId;
+    private Long brokerAccountId;
+    private String rawMessage;
+    private String clientSignalId;
+    private String receivedAt;
+}
