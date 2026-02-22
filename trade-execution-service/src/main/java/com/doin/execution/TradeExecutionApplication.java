5eff346139c2cab6a22432b8e398a97f5f626d4d
tanvir <md.tanvir.hossain.tusher@gmail.com>
2026-02-22 15:27:01 +0600

    Initialize trade-execution-service

---
+++ trade-execution-service/src/main/java/com/doin/execution/TradeExecutionApplication.java
@@ -0,0 +1,11 @@
+package com.doin.execution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeExecutionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TradeExecutionApplication.class, args);
+    }
+}
---
+++ trade-execution-service/src/test/java/com/doin/execution/TradeExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doin.execution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeExecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
